9763ce3bf696f0e8b829bb9209e1c3c0adccce81
duke-rick <dukerick2020@gmail.com>
2025-02-18 01:27:59 +0000

    Base dex class async method update

--- dex_trader/dex/base_dex.py
+++ dex_trader/dex/base_dex.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class BaseDex(ABC):
 
     @abstractmethod
-    def get_quote(self, from_token: str, to_token: str, amount: float) -> dict:
+    async def get_quote(self, from_token: str, to_token: str, amount: float) -> dict:
         """
         Retrive a price quote for swapping tokens.
         :param from_token:
@@ -15,7 +15,7 @@ class BaseDex(ABC):
         pass
 
     @abstractmethod
-    def place_order(self, order: "Order") -> str:
+    async def place_order(self, order: "Order") -> str:
         """
         Place an order on the Dex and return an order ID
         :param order:
@@ -24,7 +24,7 @@ class BaseDex(ABC):
         pass
 
     @abstractmethod
-    def cancel_order(self, order_id: str) -> bool:
+    async def cancel_order(self, order_id: str) -> bool:
         """
         Cancel an existing order given its order ID
         :param order_id:
